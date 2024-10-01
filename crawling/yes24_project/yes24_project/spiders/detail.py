ad3d801f5bc60cc19d7c9e119eb86caac3d8d7cd
khakha93 <rkwowns@naver.com>
2024-10-01 17:42:06 +0900

    작가 식별코드 추가

--- crawling/yes24_project/yes24_project/spiders/detail.py
+++ crawling/yes24_project/yes24_project/spiders/detail.py
@@ -4,35 +4,47 @@ from scrapy.http import Request
 import os
 import pandas as pd
 import csv
+import re
 
 class DetailSpider(scrapy.Spider):
     name = "detail"
     allowed_domains = ["www.yes24.com"]
-    # start_urls = ["https://www.yes24.com/Product/Goods/132381358"]
+    start_urls = ["https://www.yes24.com/Product/Goods/130503007"]
 
     def start_requests(self):
         # 모든 카테고리의 book url
         d_path = r"D:\python_project\chaekchecklab\data\basic"
 
-        for f_name in os.listdir(d_path):
+        # ^^^ for test
+        for f_name in os.listdir(d_path)[1:3]:
             # category_code
             category_code = f_name.split('.')[0]
             # print(category_code)
             
             # book list
             file_path = os.path.join(d_path, f_name)
-            book_urls = pd.read_csv(file_path).url.to_list()
+            book_urls = pd.read_csv(file_path).book_url.to_list()
             # print(file_path, len(book_urls))
 
-            for book_url in book_urls:
+            # ^^^ for test
+            for book_url in book_urls[:2]:
                 yield Request(url=book_url, meta={"cate_code": category_code}, callback=self.parse)
-                # break
-            break
 
 
     def parse(self, response):
         self.logger.info(f'Get detail information in {response.url}')
         print(f'Get detail information in {response.url}')
+
+        # 작가
+        author_dict = dict()
+        author_links = response.css('span.moreAuthLiCont>ul>li>a::attr(href)').getall()
+        if not author_links:
+            author_links = response.css('span.gd_auth>a::attr(href)').getall()
+        for author_link in author_links:
+            author_code = re.search(r'authorNo=(\d+)', author_link).group(1)
+            author_name = re.search(r'author=(.+)$', author_link).group(1)
+            author_dict[author_code] = author_name
+        # print(author_dict)
 
         # 품목 정보
         item_box = response.css('tbody.b_size')
@@ -49,11 +61,8 @@ class DetailSpider(scrapy.Spider):
         # 책 소개
         intro = "".join(response.css('div.infoWrap_txtInner > textarea')[0].css(' ::text').getall()).strip()
 
-        # print(intro)
-        with open('intro.txt', 'w', encoding='utf-8') as file:
-            file.write(str(intro))
-
-        self.save_info([[response.url, str(item_infos), str(cates), intro]], ['book_url', 'item_infos', 'cates', 'intro'], response.meta['cate_code'])
+        # 저장
+        self.save_info([[response.url, author_dict, str(item_infos), str(cates), intro]], ['book_url', 'author_dict', 'item_infos', 'cates', 'intro'], response.meta['cate_code'])
 
     def save_info(self, detail_info_list, columns, cate_code):
         # print(detail_info_list)
@@ -65,5 +74,7 @@ class DetailSpider(scrapy.Spider):
             df.to_csv(full_path, index=False, mode='w', quoting=csv.QUOTE_MINIMAL)
         else:
             df.to_csv(full_path, index=False, mode='a', header=False, quoting=csv.QUOTE_MINIMAL)
+
+        print(f"Saved in basic_{cate_code}.csv")
 
         return
